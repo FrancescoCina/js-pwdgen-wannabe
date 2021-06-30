365991c97481486af69797aecf6ae7da75fe285d
FrancescoCina <francesco.cina04@gmail.com>
2021-06-30 14:45:13 +0200

    Aggiunto margine css al tag <p>, fatto esercizio su età come in classe

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@
 
  */
 
+
+
+
 var passwordElement = document.getElementById("password");
 console.log("elemento html", passwordElement);
 
@@ -24,3 +27,33 @@ var lastCharacter = ("21");
 console.log(lastCharacter);
 
 passwordElement.innerHTML = ("La tua password è: ") + name + surname + preferColor + lastCharacter;
+
+
+
+
+
+/* 
+
+ESERCIZIO ETA'
+
+Chiediamo all'utente la sua età e forniamogli la sua data di nascita:
+
+variabile per età
+chiediamo età
+variabile per l'anno in cui siamo
+stampiamo data di nascita
+
+*/
+
+/* var age = prompt("Quanti anni hai?");
+console.log(age);
+var currentYear = ("2021");
+console.log(currentYear);
+var yearElement = document.getElementById("birthYear");
+console.log(yearElement);
+
+
+yearElement.innerHTML = currentYear - age;
+ 
+
+ */
