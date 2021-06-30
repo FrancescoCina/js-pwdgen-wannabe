22fded9c1a96d348d7b142534dd2df35dc36ce66
FrancescoCina <francesco.cina04@gmail.com>
2021-06-30 14:28:08 +0200

    Fatte le diverse variabili e stampate sia in console che su html l'esercizio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-console.log("js ok");
 
+/* 
+    STEP:
+    Chiedi all’utente il suo nome,
+    poi chiedi il suo cognome,
+    poi chiedi il suo colore preferito
+    Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+ */
 
+var passwordElement = document.getElementById("password");
+console.log("elemento html", passwordElement);
 
+var name = prompt("Inserisci qui il tuo nome");
+console.log("Nome", name);
 
+var surname = prompt("Iserisci qui il tuo cognome");
+console.log("Cognome", surname);
 
+var preferColor = prompt("Qual è il tuo colore preferito?");
+console.log("Colore", preferColor);
+
+var lastCharacter = ("21")
+
+passwordElement.innerHTML = ("La tua password è: ") + name + surname + preferColor + lastCharacter;
